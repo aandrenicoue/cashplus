cd6876294f0a88017c9f2a89ef5bddcd8e131fe8
nabil kpossa <nabil.kpossa51@gmail.com>
2023-04-21 11:22:24 +0100

    commit

---
+++ src/main/java/com/pegase/cashplus/account/Account.java
@@ -0,0 +1,14 @@
+package com.pegase.cashplus.account;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class Account {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private
+
+
+}
